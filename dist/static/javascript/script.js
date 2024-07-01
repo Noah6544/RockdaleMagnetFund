489b99eabd875979a6a462de8f52130a7ba8bf0e
Noah Buchanan <buchanannoah100@gmail.com>
2024-06-30 22:51:28 -0400

    I needa go train. left off on teh contact.html trying to style everything properly. try to seperate all other page content into antoher div to keep the page header constant

--- dist/static/javascript/script.js
+++ dist/static/javascript/script.js
@@ -7,14 +7,14 @@ const mobileNavbar = document.getElementById("mobileNavbar")
 var isOpen = false;
 
 menu.addEventListener("click", function(){
-  mobileNavbar.classList.remove("opacity-0");
+  mobileNavbar.classList.remove("opacity-0","pointer-events-none");
 
   if(isOpen){
-    mobileNavbar.classList.add("opacity-0")
+    mobileNavbar.classList.add("opacity-0","pointer-events-none")
     isOpen = false;
   }
   else{
-    mobileNavbar.classList.remove("opacity-0")
+    mobileNavbar.classList.remove("opacity-0","pointer-events-none")
     isOpen = true;
   }
 })
