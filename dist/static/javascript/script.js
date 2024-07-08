518dd7a9a32ab62de2baa54214bfe31d1c444ba9
Noah Buchanan <buchanannoah100@gmail.com>
2024-07-08 11:35:36 -0400

    Saving as I did some menu modifcatiosn before i change some stuff

--- dist/static/javascript/script.js
+++ dist/static/javascript/script.js
@@ -13,24 +13,48 @@ const CheckItemInView = new IntersectionObserver((entries) => { //figure out thi
   
 });
 
+function showMobileNavbarItems(show){ //DRY NOT WET
+  if(show){
+    var delay = 0;
+    mobileNavbarItems.forEach(element => {
+      delay += .08;
+      element.style.cssText = "transition-delay: " + delay + "s;"
+      element.classList.remove("mobileNavbarListHidden"); 
+      element.classList.add("mobileNavbarListShow"); // spent 1 hour debugging but i forgot i had a '.classname'
+    });
+  }
+  else{
+    var delay = 0;
+    mobileNavbarItems.forEach(element => {
+      element.style.cssText = "transition-delay: " + delay + "s;"
+      element.classList.remove("mobileNavbarListShow"); 
+      element.classList.add("mobileNavbarListHidden"); 
+      delay += .05;
+      console.log(element);
+    });
+  }
+
+}
+
 
 // GETTING DOM ITEMS
 const onScrollItems = document.querySelectorAll(".onScrollHidden");
 const menu = document.getElementById("menu");
-const mobileNavbar = document.getElementById("mobileNavbar")
+const mobileNavbar = document.getElementById("mobileNavbar");
+const mobileNavbarItems = document.querySelectorAll('.mobileNavbarListHidden');
 
 //RUNNING CODE
 var isOpen = false;
-
 menu.addEventListener("click", function(){
   mobileNavbar.classList.remove("opacity-0","pointer-events-none");
-
+  showMobileNavbarItems(true);
   if(isOpen){
-    mobileNavbar.classList.add("opacity-0","pointer-events-none")
+    mobileNavbar.classList.add("opacity-0","pointer-events-none");
+    showMobileNavbarItems(false);
     isOpen = false;
   }
   else{
-    mobileNavbar.classList.remove("opacity-0","pointer-events-none")
+    mobileNavbar.classList.remove("opacity-0","pointer-events-none");
     isOpen = true;
   }
 });
