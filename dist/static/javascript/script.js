fece88db5ff5b580d38d05bb5e40fe0ff254939d
Noah Buchanan <noah6544@gmail.com>
2024-07-07 23:56:36 -0400

    Todays not been the best. struggled to get work done and out of bed because of challenge covid quarantine-ing. saat aat pc to work but only started reaally working about an hour or so ago, commiting before midnight for hte github green

--- dist/static/javascript/script.js
+++ dist/static/javascript/script.js
@@ -1,9 +1,27 @@
+//FUNCTIONS
+
+const CheckItemInView = new IntersectionObserver((element) => { //figure out this issue and why it's not working
+    console.log(element)
+    if(element.isIntersecting){
+      element.classList.add("onScrollView");
+      console.log("added!s");
+      return true;
+    }
+    else{
+      console.log("asdf")
+      return false;
+    }
+  ;
+  
+});
+
+
 // GETTING DOM ITEMS
-
-//RUNNING CODE
-
+const onScrollItems = document.querySelectorAll(".onScrollHidden");
 const menu = document.getElementById("menu");
 const mobileNavbar = document.getElementById("mobileNavbar")
+
+//RUNNING CODE
 var isOpen = false;
 
 menu.addEventListener("click", function(){
@@ -17,4 +35,6 @@ menu.addEventListener("click", function(){
     mobileNavbar.classList.remove("opacity-0","pointer-events-none")
     isOpen = true;
   }
-})
+});
+
+onScrollItems.forEach(element => CheckItemInView.observe(element));
